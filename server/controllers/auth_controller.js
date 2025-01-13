c34207cdab6ce3008d9d5415449da6173499e531
Sushant123450 <Sushant123450.sk@gmail.com>
2025-01-14 01:36:31 +0530

    Forth Commit

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -5,8 +5,9 @@ const jwt = require("jsonwebtoken");
 exports.register = async (req, res) => {
 	try {
 		let { name, email, password, role } = req.body;
-		email = email.toLowerCase();
-		// console.log(name, em/ail, password, role);
+		email = email.toLowerCase().trim();
+		name = name.toLowerCase().trim();
+		// console.log(name, email, password, role);
 
 		const exisitingUser = await User.findOne({ email: email });
 		if (exisitingUser) {
@@ -53,7 +54,8 @@ exports.login = async (req, res) => {
 
 	try {
 		let { email, password } = req.body;
-		email = email.toLowerCase();
+		email = email.toLowerCase().trim();
+		name = name.toLowerCase().trim();
 		// console.log(req.body);
 
 		if (!email || !password) {
@@ -86,68 +88,6 @@ exports.login = async (req, res) => {
 
 			const options = {
 				expries: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-				httpOnly: true,
-			};
-
-			res.cookie("token", token, options).status(200).json({
-				success: "success",
-				token,
-				exisitingUser,
-				message: "User LoggedIn Successfully",
-			});
-		} else {
-			return res.status(403).json({
-				success: "false",
-				message: "Invalid Password",
-			});
-		}
-	} catch (e) {
-		console.log(e);
-		return res.status(500).json({
-			success: "failed",
-			message: "Login Failiure",
-		});
-	}
-};
-
-exports.login = async (req, res) => {
-	console.log("Login called");
-
-	try {
-		let { email, password } = req.body;
-		email = email.toLowerCase();
-		// console.log(req.body);
-
-		if (!email || !password) {
-			return res.status(400).json({
-				success: "failed",
-				message: "Please Fill all Details",
-			});
-		}
-
-		const exisitingUser = await User.findOne({ email: email });
-		if (!exisitingUser) {
-			return res.status(401).json({
-				success: "failed",
-				message: "User Not Found",
-			});
-		}
-		const payload = {
-			email: exisitingUser.email,
-			id: exisitingUser._id,
-			role: exisitingUser.role,
-		};
-
-		if (await bcrypt.compare(password, exisitingUser.password)) {
-			let token = jwt.sign(payload, process.env.JWT_SECRET, {
-				expiresIn: "2h",
-			});
-
-			exisitingUser.token = token;
-			exisitingUser.password = undefined;
-
-			const options = {
-				expries: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
 				httpOnly: true,
 			};
 
